3c6c68b6e56e7b7165e0d85f98418c397c1dc137
sazzadkhan20 <sazzad53913@gmail.com>
2026-01-21 01:10:34 +0600

    Fix some Bugs and Implement Asynchronous API

--- Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9be835e12bde8b2b21f7420c2a39903eccbbc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa4623dbf551333d98c04ca07f08ea392ac33d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
